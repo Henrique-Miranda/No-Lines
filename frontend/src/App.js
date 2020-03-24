bf474c21e8ca0356d09e7abf89a060cd1a2116b9
Henrique Miranda <henrique@disroot.org>
2020-03-24 19:26:06 -0300

    Send Frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo2.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Comming Soon.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
